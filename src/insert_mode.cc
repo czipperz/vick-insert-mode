89f0936abb1c7a406a3e1affb890ffa205893d91
Czipperz <czipperz@gmail.com>
2015-10-05 18:36:26 -0700

    Rename replace_text mode to replace and refresh screen after keypress

--- src/insert_mode.cc
+++ src/insert_mode.cc
@@ -9,16 +9,22 @@ void enter_insert_mode(contents& contents, boost::optional<int>) {
     while((ch = getch()) != _escape) {
         contents.cont[contents.y].insert(contents.x, 1, ch);
         mvf(contents);
+        if(get_contents().refresh) {
+            print_contents(get_contents());
+        }
     }
 }
 
-void enter_replace_insert_mode(contents& contents, boost::optional<int>) {
+void enter_replace_mode(contents& contents, boost::optional<int>) {
     char ch;
     while((ch = getch()) != _escape) {
         if(contents.x >= contents.cont[contents.y].size()) {
             contents.cont[contents.y].push_back(ch);
         } else {
             contents.cont[contents.y][contents.x] = ch;
+        }
+        if(get_contents().refresh) {
+            print_contents(get_contents());
         }
     }
 }
--- src/insert_mode.hh
+++ src/insert_mode.hh
@@ -1,4 +1,4 @@
 #include "../../../src/contents.hh"
 
-void enter_insert_mode      (contents&, boost::optional<int> = boost::none);
-void enter_replace_text_mode(contents&, boost::optional<int> = boost::none);
+void enter_insert_mode (contents&, boost::optional<int> = boost::none);
+void enter_replace_mode(contents&, boost::optional<int> = boost::none);
